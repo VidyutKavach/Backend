a873ba1cbcf0edeab2250bbed02656aa2a24226a
gracygupta <hzgracy@gmail.com>
2024-03-14 11:45:22 +0530

    create nanogrid model

---
+++ src/models/nanogrid.ts
@@ -0,0 +1,42 @@
+import { Schema, model, Document, ObjectId } from "mongoose";
+
+interface Component {
+  type: string;
+  componentID: string;
+}
+
+export interface NanogridDocument extends Document {
+  nanogridID: string;
+  name: string;
+  components: Component[];
+}
+
+interface Nanogrid {
+  nanogridID: string;
+  name: string;
+  components: Component[];
+}
+
+const nanogridSchema = new Schema<Nanogrid>(
+  {
+    nanogridID: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      reuired: true,
+      unique: true,
+    },
+    components: {
+      type: [{ type: String }],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Nanogrid = model<Nanogrid>("nanogrids", nanogridSchema);
+
+export default Nanogrid;
