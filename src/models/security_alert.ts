33c2501a6d7710bcf19506d2e84dcbc7ed37d42e
gracygupta <hzgracy@gmail.com>
2024-03-14 11:46:48 +0530

    create security alert model

---
+++ src/models/security_alert.ts
@@ -0,0 +1,61 @@
+import { Schema, model, Document, ObjectId } from "mongoose";
+
+export interface SecurityAlertDocument extends Document {
+  alert_id: number;
+  read: boolean;
+  type: string;
+  severity: "Critical" | "High" | "Medium" | "Low";
+  attacker_ip: string;
+  action: "blocked" | "on-surviellance";
+  description: string;
+}
+
+interface SecurityAlert {
+  alert_id: number;
+  read: boolean;
+  type: string;
+  severity: "Critical" | "High" | "Medium" | "Low";
+  attacker_ip: string;
+  action: "blocked" | "on-surviellance";
+  description: string;
+}
+
+const securityAlertSchema = new Schema<SecurityAlert>(
+  {
+    alert_id: {
+      type: Number,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    severity: {
+      type: String,
+      enum: ["Critical", "High", "Medium", "Low"],
+    },
+    attacker_ip: {
+      type: String,
+    },
+    action: {
+      type: String,
+      enum: ["blocked", "on-surviellance"],
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const SecurityAlert = model<SecurityAlert>(
+  "security_alerts",
+  securityAlertSchema
+);
+
+export default SecurityAlert;
