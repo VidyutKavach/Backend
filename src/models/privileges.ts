899c7db4e4ff3be0a91352c8df8ef108685cd84c
gracygupta <hzgracy@gmail.com>
2024-03-14 11:33:34 +0530

    create privilege model

---
+++ src/models/privileges.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document, ObjectId } from "mongoose";
+
+export interface PrivilegeDocument extends Document {
+  name: string;
+  description: string | undefined;
+}
+
+interface Privilege {
+  name: string;
+  description: string | undefined;
+}
+
+const privilegeSchema = new Schema<Privilege>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const Privilege = model<Privilege>("privileges", privilegeSchema);
+
+export default Privilege;
