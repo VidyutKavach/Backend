55a7a1475b890945d7629ea4ac965736066c160e
gracygupta <hzgracy@gmail.com>
2024-03-14 11:43:00 +0530

    create user model

---
+++ src/models/user.ts
@@ -0,0 +1,68 @@
+import { Schema, model, Document, ObjectId } from "mongoose";
+
+export interface UserDocument extends Document {
+  empID: string;
+  username: string;
+  role: ObjectId;
+  email: string;
+  about: string | undefined;
+  password: string;
+  isVerified: boolean | undefined;
+}
+
+interface User {
+  empID: string;
+  username: string;
+  role: ObjectId;
+  email: string;
+  about: string | undefined;
+  password: string;
+  isVerified: boolean | undefined;
+}
+
+const userSchema = new Schema<User>(
+  {
+    empID: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (value: string) => {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return emailRegex.test(value);
+        },
+        message: (props) => `${props.value} is not a valid email address`,
+      },
+    },
+    about: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model<User>("user", userSchema);
+
+export default User;
