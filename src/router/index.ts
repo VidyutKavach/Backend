76b6d1ca405b2c7b02ccb898a6a6faa502b98636
gracygupta <hzgracy@gmail.com>
2024-03-14 17:08:41 +0530

    create dashboard routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router();
 import admin from "./admin";
 import user from "./user";
+import dashboard from "./dashboard";
 
 router.use("/admin", admin);
 router.use("/user", user);
+router.use("/dashboard", dashboard);
 
 export default router;
